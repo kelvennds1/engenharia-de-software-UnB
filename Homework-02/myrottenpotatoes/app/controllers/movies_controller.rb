debfae009672ce67775c70bff51258fa4b908b7f
Kelven Cristian <kelvencrstn@gmail.com>
2025-09-30 17:48:40 +0000

    Adiciona ações de criação, edição, atualização e exclusão ao MoviesController

--- Homework-02/myrottenpotatoes/app/controllers/movies_controller.rb
+++ Homework-02/myrottenpotatoes/app/controllers/movies_controller.rb
@@ -7,4 +7,39 @@ class MoviesController < ApplicationController
     id = params[:id] # obtém o id do parâmetro da URL
     @movie = Movie.find(id)
   end
+
+  def new
+    
+  end
+
+  def create
+    @movie = Movie.create!(movie_params)
+    flash[:notice] = "#{@movie.title} was successfully created."
+    redirect_to movies_path
+  end
+
+  def edit
+    @movie = Movie.find params[:id]
+  end
+
+  def update
+    @movie = Movie.find params[:id]
+    @movie.update!(movie_params)
+    flash[:notice] = "#{@movie.title} was successfully updated."
+    redirect_to movie_path(@movie)
+  end
+
+  def destroy
+    @movie = Movie.find params[:id]
+    @movie.destroy
+    flash[:notice] = "#{@movie.title} was deleted."
+    redirect_to movies_path
+  end
+
+  private
+
+  def movie_params
+    params.require(:movie).permit(:title, :rating, :release_date)
+  end
+
 end
